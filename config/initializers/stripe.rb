36879fb2d7f43219cab2dfb2598a9c0dfd517526
Dinesh <dineshdhoom@gmail.com>
2012-04-01 00:24:20 +0530

    init repo

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = "6c42YXvkym6gUvdDpfFRiYCv4Ls0BfCe"
+STRIPE_PUBLIC_KEY = "pk_quRkUHSCoG1ApDUdLXqSXKoF0fMTA"
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Plan.create!(:name => "Basic", :price => 1)
+Plan.create!(:name => "Plus", :price => 2)
+Plan.create!(:name => "Pro", :price => 3)
