3d7d4a466415f61ccabbc4a3180cc50c7d86c598
Ranjeet <ranjeet@ranjeet-desktop.(none)>
2012-04-01 18:02:57 +0530

    Jobs form

--- db/migrate/20120401070841_create_jobs.rb
+++ db/migrate/20120401070841_create_jobs.rb
@@ -1,7 +1,16 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-      t.text :job_detail
+      t.string :customer_name
+      t.text :address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.integer :phone
+      t.string :email
+      t.string :insurance_company
+      t.string :adjuster
+      t.integer :claim_number
       t.timestamps
     end
   end
