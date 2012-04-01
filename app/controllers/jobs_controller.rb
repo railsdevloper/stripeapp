755929a0966ecc36ac309fedb3af5b30d33eec15
Ranjeet <ranjeet@ranjeet-desktop.(none)>
2012-04-01 14:34:24 +0530

    Create and show functionality for job and validations

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,11 +5,17 @@ class JobsController < ApplicationController
   end
 
   def show
-    
+    @job = Job.find(params[:id])
+    @inventory = @job.inventories.new
   end
 
   def create
-    
+    @job = Job.create(params[:job])
+    if @job.save
+      redirect_to job_path(@job) 
+    else
+     render :action => "new"
+    end
   end
 
   def new
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
+  validates :name, :job_detail, :presence => true 
   has_many :inventories
 end
