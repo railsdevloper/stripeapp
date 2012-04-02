169bd1d88d054f47da3b422d99909837d3c5678b
Ranjeet <ranjeet@ranjeet-desktop.(none)>
2012-04-02 19:32:32 +0530

    Create the Signup Page when user clicks on any available plans

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,18 @@ class Subscription < ActiveRecord::Base
   belongs_to :plan
   validates_presence_of :plan_id
   validates_presence_of :email
-  
+  validates_confirmation_of :password
+  validates_presence_of :password_confirmation
   attr_accessor :stripe_card_token
   
   def save_with_payment
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.last4 = customer[:active_card][:last4]
+      self.cardholder_name = customer[:active_card][:name]
+      self.card_type = customer[:active_card][:type]
+      self.exp_month = customer[:active_card][:exp_month]
+      self.exp_year = customer[:active_card][:exp_year]
       self.stripe_customer_token = customer.id
       save!
     end
