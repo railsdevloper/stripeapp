4946630b0556ac530965e19e79da37f3945c8029
Ranjeet <ranjeet@ranjeet-desktop.(none)>
2012-04-05 21:01:02 +0530

    Sign in functionality and status of jobs and inventory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(Subscription)
+        jobs_path
+      else
+        super
+      end
+  end
+
+
+
 end
