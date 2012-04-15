acf054f6a18aeaa2ea7e44f46281b5a63424fa77
Dinesh <dineshdhoom@gmail.com>
2012-04-15 17:35:33 +0530

    Adding debugging statements in session controller to check devise login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,9 @@ class SessionsController < DeviseController
   # POST /resource/sign_in
   def create
     resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    set_flash_message(:notice, :signed_in) if is_navigational_format?    
     sign_in(resource_name, resource)
+    puts "resource====================#{resource.inspect}-----------#{resource_name.inspect}DDDDDDDDDDDDDDDDDDDD"
     respond_with resource, :location => after_sign_in_path_for(resource)
   end
 
