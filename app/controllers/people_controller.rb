8c6e5c3a24ebf779d49e31a5d5ec29336f4590d3
Dinesh <dineshdhoom@gmail.com>
2012-04-18 21:19:53 +0530

    Done some fixes in people update and show form

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,12 +1,23 @@
 class PeopleController < ApplicationController
+  
+  before_filter :find_insurance_company, :only => [:new, :edit]
+
   def index
     @person = current_subscription.people.all
     
   end
 
+  def edit
+    @person = Person.find(params[:id])
+    render :template => "people/new.html.haml"
+  end
+
   def new 
     @person = current_subscription.people.new
-    @insurance_companies = current_subscription.insurance_companies || []
+  end
+
+  def show
+    @person = Person.find(params[:id])
   end
 
   def create
@@ -18,8 +29,20 @@ class PeopleController < ApplicationController
     end
   end
 
-  def show
+  def update
     @person = Person.find(params[:id])
+    if @person.update_attributes(params[:person])
+      redirect_to people_path(current_subscription.username)
+    else
+      render :action => "show"
+    end
   end
+
+  private 
+  
+  def find_insurance_company
+    @insurance_companies = current_subscription.insurance_companies || []
+  end
+
 
 end
